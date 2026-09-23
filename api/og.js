e01bac3da6090fe1cb62d559d25555c37ef68dd7
danielaranadj-prog <danielaranadj@gmail.com>
2026-09-23 10:53:07 -0700

    fix(seo): add X-Robots-Tag: noindex to og.js to prevent API URLs from being indexed by Google

--- api/og.js
+++ api/og.js
@@ -664,6 +664,7 @@ ${bodyContent}
     headers: {
       "Content-Type": "text/html; charset=utf-8",
       "Cache-Control": "public, s-maxage=3600, stale-while-revalidate=86400",
+      "X-Robots-Tag": "noindex, follow",
     },
   });
 }
